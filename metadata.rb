97c695f9fd3478c4289aee8dec73d0b17e25cd1f
pigram86 <todd@toddpigram.com>
2014-01-20 08:53:41 -0500

    berks

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,7 @@ maintainer_email 'todd@toddpigram.com'
 license          'Apache 2.0'
 description      'Installs/Configures w2k8_rds'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
+depends          "windows"
+depends          "powershell"
 
